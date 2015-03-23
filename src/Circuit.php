98979a1d60fbfd987410c231e85c19b334082a66
Steve Corona <thestevecorona@gmail.com>
2015-03-23 07:58:36 -0700

    stub out methods

--- src/Circuit.php
+++ src/Circuit.php
@@ -1,5 +1,19 @@
 <?php
 
 class Circuit {
+
+  public $name;
+
+  public function __construct($circuit_name) {
+    $this->name = $circuit_name;
+  }
+
+  public function isCircuitOpen() {
+    return true;
+  }
+
+  public function isCircuitClosed() {
+    return false;
+  }
   
 }
